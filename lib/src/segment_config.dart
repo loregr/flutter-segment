e4d8a0cf8aa5798aa33beabfb875df53fb00f6f4
Lorenzo Greco <lorenzo.greco@primarybid.com>
2023-05-10 13:51:07 +0100

    Updates SegmentConfig

--- lib/src/segment_config.dart
+++ lib/src/segment_config.dart
@@ -2,6 +2,8 @@ class SegmentConfig {
   SegmentConfig({
     required this.writeKey,
     this.trackApplicationLifecycleEvents = false,
+    this.trackPushNotifications = false,
+    this.trackDeepLinks = false,
     this.amplitudeIntegrationEnabled = false,
     this.appsflyerIntegrationEnabled = false,
     this.debug = false,
@@ -9,6 +11,8 @@ class SegmentConfig {
 
   final String writeKey;
   final bool trackApplicationLifecycleEvents;
+  final bool trackPushNotifications;
+  final bool trackDeepLinks;
   final bool amplitudeIntegrationEnabled;
   final bool appsflyerIntegrationEnabled;
   final bool debug;
@@ -17,6 +21,8 @@ class SegmentConfig {
     return {
       'writeKey': writeKey,
       'trackApplicationLifecycleEvents': trackApplicationLifecycleEvents,
+      'trackPushNotifications': trackPushNotifications,
+      'trackDeepLinks': trackDeepLinks,
       'amplitudeIntegrationEnabled': amplitudeIntegrationEnabled,
       'appsflyerIntegrationEnabled': appsflyerIntegrationEnabled,
       'debug': debug,
